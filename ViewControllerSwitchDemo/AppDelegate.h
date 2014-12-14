859d7b6fb4d92769bc34ce21bf603a48aa7e9514
臧 立涛 <zanglitao@zoumatoMacBook-Pro.local>
2014-12-14 14:56:07 +0800

    Initial Commit

---
+++ ViewControllerSwitchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewControllerSwitchDemo
+//
+//  Created by 臧 立涛 on 14-12-14.
+//  Copyright (c) 2014年 臧 立涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewControllerSwitchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewControllerSwitchDemo
+//
+//  Created by 臧 立涛 on 14-12-14.
+//  Copyright (c) 2014年 臧 立涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
